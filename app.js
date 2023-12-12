e8c2eb7757fbcffe63ad5400ee65bdd3adb9d980
César Márquez <cesar.marquezsal@gmail.com>
2023-12-12 09:44:47 -0400

    language fully working

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
-// Elementos del DOM
 const flagsElement = document.getElementById("flags");
 const textsToChange = document.querySelectorAll("[data-section]");
 
-
-const changeLanguage = async (language) => {
-    const requestJson = await fetch(`./languages/${language}.json`);
-    const texts = await requestJson.json();
-
-    // Cambia el nombre de la variable a textElement
-    for (const textElement of textsToChange) {
-        
-        const section = textElement.dataset.section;
-        const value = textElement.dataset.value;
-
-        // Usa textElement en lugar de textsToChange
-        textElement.innerHTML=texts[section][value];
-
-    }
-    
-};
-
-
-// Cambiar el tema
-const cambiarTema = () => {
-    const temaActual = document.documentElement.getAttribute('data-theme');
-    const nuevoTema = temaActual === 'oscuro' ? 'claro' : 'oscuro';
-    document.documentElement.setAttribute('data-theme', nuevoTema);
-
-    let icono = document.getElementById("iconoTema");
-    icono.classList.toggle("fa-sun");
-    icono.classList.toggle("fa-moon");
-}
-
 // Cambiar el idioma
-const cambiarIdioma = () => {
-    const boton = document.getElementById("textoIdioma");
-    boton.innerHTML = boton.innerHTML === "ES" ? "EN" : "ES";
-}
+const changeLanguage = async () => {
+    let currentLanguage = document.getElementById("textoIdioma").innerText; // Obtiene el idioma actual
+    let nextLanguage; // Variable para almacenar el próximo idioma
+
+    // Determina el próximo idioma en base al actual
+    if (currentLanguage === "ES") {
+      nextLanguage = "en";
+    } else if (currentLanguage === "EN") {
+      nextLanguage = "es";
+    }
+
+    // Realiza la misma lógica de carga de textos
+    try {
+        // Imprime la URL del archivo JSON
+        console.log(`Petición realizada a: ./languages/${nextLanguage}.json`);
+
+        // Inicializa la variable requestJson
+        const requestJson = await fetch(`./languages/${nextLanguage}.json`);
+
+        if (requestJson.status !== 200) {
+            throw new Error(`No se pudo cargar el archivo de idioma: ${requestJson.status}`);
+        }
+
+        // Carga los textos del archivo JSON
+        const texts = await requestJson.json();
+
+        // Actualiza la página con los textos del nuevo idioma
+        for (const textElement of textsToChange) {
+            const section = textElement.dataset.section;
+            const value = textElement.dataset.value;
+
+            textElement.innerText = texts[section][value];
+        }
+
+        // Actualiza el texto del botón con el nuevo idioma
+        const boton = document.getElementById("textoIdioma");
+        boton.innerText = nextLanguage === "es" ? "ES" : "EN";
+    } catch (error) {
+        console.error(`Error al cambiar el idioma:`, error);
+    }
+  };
 
 // Eventos
 flagsElement.addEventListener("click", (e) => {
@@ -47,4 +53,15 @@ window.onscroll = () => {
     const scrollPosY = window.pageYOffset | document.body.scrollTop;
     const gradientValue = Math.min(0.5, scrollPosY/100);
     document.querySelector('.sidemenu').style.background = `linear-gradient(to bottom, rgba(0, 0, 0, ${gradientValue}), transparent)`;
+}
+
+// Cambiar el tema
+const cambiarTema = () => {
+    const temaActual = document.documentElement.getAttribute('data-theme');
+    const nuevoTema = temaActual === 'oscuro' ? 'claro' : 'oscuro';
+    document.documentElement.setAttribute('data-theme', nuevoTema);
+
+    let icono = document.getElementById("iconoTema");
+    icono.classList.toggle("fa-sun");
+    icono.classList.toggle("fa-moon");
 }
